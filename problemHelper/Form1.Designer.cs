6f656d96f7c755aa4b827c8ad78da21c8addf926
nullified-void <53339791+nullified-void@users.noreply.github.com>
2019-10-10 09:33:42 +0300

    Add files via upload

---
+++ problemHelper/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace problemHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ProblemHeaderLabel = new System.Windows.Forms.Label();
+            this.ProblemTextLabel = new System.Windows.Forms.Label();
+            this.ProblemHeader = new System.Windows.Forms.TextBox();
+            this.ProblemText = new System.Windows.Forms.TextBox();
+            this.SendBTN = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.сообщитьОПроблемеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьПриложениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ProblemHeaderLabel
+            // 
+            this.ProblemHeaderLabel.AutoSize = true;
+            this.ProblemHeaderLabel.Location = new System.Drawing.Point(113, 9);
+            this.ProblemHeaderLabel.Name = "ProblemHeaderLabel";
+            this.ProblemHeaderLabel.Size = new System.Drawing.Size(116, 13);
+            this.ProblemHeaderLabel.TabIndex = 0;
+            this.ProblemHeaderLabel.Text = "Заголовок проблемы";
+            // 
+            // ProblemTextLabel
+            // 
+            this.ProblemTextLabel.AutoSize = true;
+            this.ProblemTextLabel.Location = new System.Drawing.Point(113, 48);
+            this.ProblemTextLabel.Name = "ProblemTextLabel";
+            this.ProblemTextLabel.Size = new System.Drawing.Size(132, 13);
+            this.ProblemTextLabel.TabIndex = 1;
+            this.ProblemTextLabel.Text = "Опишите вашу проблему";
+            // 
+            // ProblemHeader
+            // 
+            this.ProblemHeader.Location = new System.Drawing.Point(12, 25);
+            this.ProblemHeader.Name = "ProblemHeader";
+            this.ProblemHeader.Size = new System.Drawing.Size(313, 20);
+            this.ProblemHeader.TabIndex = 2;
+            // 
+            // ProblemText
+            // 
+            this.ProblemText.Location = new System.Drawing.Point(12, 64);
+            this.ProblemText.Multiline = true;
+            this.ProblemText.Name = "ProblemText";
+            this.ProblemText.Size = new System.Drawing.Size(313, 194);
+            this.ProblemText.TabIndex = 2;
+            // 
+            // SendBTN
+            // 
+            this.SendBTN.Location = new System.Drawing.Point(136, 264);
+            this.SendBTN.Name = "SendBTN";
+            this.SendBTN.Size = new System.Drawing.Size(75, 23);
+            this.SendBTN.TabIndex = 3;
+            this.SendBTN.Text = "Отправить";
+            this.SendBTN.UseVisualStyleBackColor = true;
+            this.SendBTN.Click += new System.EventHandler(this.SendBTN_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сообщитьОПроблемеToolStripMenuItem,
+            this.закрытьПриложениеToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(202, 48);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // сообщитьОПроблемеToolStripMenuItem
+            // 
+            this.сообщитьОПроблемеToolStripMenuItem.Name = "сообщитьОПроблемеToolStripMenuItem";
+            this.сообщитьОПроблемеToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.сообщитьОПроблемеToolStripMenuItem.Text = "Сообщить о проблеме";
+            this.сообщитьОПроблемеToolStripMenuItem.Click += new System.EventHandler(this.сообщитьОПроблемеToolStripMenuItem_Click);
+            // 
+            // закрытьПриложениеToolStripMenuItem
+            // 
+            this.закрытьПриложениеToolStripMenuItem.Name = "закрытьПриложениеToolStripMenuItem";
+            this.закрытьПриложениеToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.закрытьПриложениеToolStripMenuItem.Text = "Закрыть приложение";
+            this.закрытьПриложениеToolStripMenuItem.Click += new System.EventHandler(this.закрытьПриложениеToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 307);
+            this.Controls.Add(this.SendBTN);
+            this.Controls.Add(this.ProblemText);
+            this.Controls.Add(this.ProblemHeader);
+            this.Controls.Add(this.ProblemTextLabel);
+            this.Controls.Add(this.ProblemHeaderLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ProblemHeaderLabel;
+        private System.Windows.Forms.Label ProblemTextLabel;
+        private System.Windows.Forms.TextBox ProblemHeader;
+        private System.Windows.Forms.TextBox ProblemText;
+        private System.Windows.Forms.Button SendBTN;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem сообщитьОПроблемеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem закрытьПриложениеToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ problemHelper/Form1.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace problemHelper
+{
+    public partial class Form1 : Form
+    {
+        public class ReqInfo
+        {
+            public string CPU = "";
+            public string OS = "";
+            public string DateofReq = "";
+            public string Header = "";
+            public string probleminfo = "";
+            public string currentuser = "";
+            public string pcname = "";
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            notifyIcon1 = new NotifyIcon();
+            notifyIcon1.Icon = SystemIcons.Exclamation;
+            notifyIcon1.Visible = true;
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+
+        }
+
+       
+        private bool closing = true;
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = closing;
+            this.Hide();
+        }
+
+        private void сообщитьОПроблемеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void закрытьПриложениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            closing = false;
+            Application.Exit();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //this.Hide();
+        }
+
+        private void SendBTN_Click(object sender, EventArgs e)
+        {
+            ReqInfo package = new ReqInfo();
+            RegistryKey processor_name = Registry.LocalMachine.OpenSubKey(@"Hardware\Description\System\CentralProcessor\0", RegistryKeyPermissionCheck.ReadSubTree);
+            if (processor_name != null)
+            {
+                if (processor_name.GetValue("ProcessorNameString") != null)
+                {
+                    package.CPU = Convert.ToString((processor_name.GetValue("ProcessorNameString"))); 
+                }
+            }
+            package.DateofReq = DateTime.Now.ToString("MM/dd/yyyy HH:mm");
+            package.Header = ProblemHeader.Text;
+            package.probleminfo = ProblemText.Text;
+            package.OS = getOSInfo() + '/' + Environment.OSVersion;
+            package.currentuser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            package.pcname = Environment.MachineName;
+            string json = JsonConvert.SerializeObject(package);
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:59735/api/values/add"); // сюда нужно будет вставить адрес апи сервера, принимающего запрос
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            //var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse(); // с этим нужно будет работать если нужно принимать ответ с веб-сервера.
+            MessageBox.Show("Ваш запрос принят в обработку. Диалоговое окно закроется автоматически.", "", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            this.Close();
+        }
+        string getOSInfo()
+        {
+            OperatingSystem os = Environment.OSVersion;
+            Version vs = os.Version;
+            string operatingSystem = "";
+            if (os.Platform == PlatformID.Win32NT)
+            {
+                switch (vs.Major)
+                {
+                    case 3:
+                        operatingSystem = "NT 3.51";
+                        break;
+                    case 4:
+                        operatingSystem = "NT 4.0";
+                        break;
+                    case 5:
+                        if (vs.Minor == 0)
+                            operatingSystem = "2000";
+                        else
+                            operatingSystem = "XP";
+                        break;
+                    case 6:
+                        if (vs.Minor == 0)
+                            operatingSystem = "Vista";
+                        else if (vs.Minor == 1)
+                            operatingSystem = "7";
+                        else if (vs.Minor == 2)
+                            operatingSystem = "8";
+                        else
+                            operatingSystem = "8.1";
+                        break;
+                    case 10:
+                        operatingSystem = "10";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (operatingSystem != "")
+            {
+                operatingSystem = "Windows " + operatingSystem;
+                if (os.ServicePack != "")
+                {
+                    operatingSystem += " " + os.ServicePack;
+                }
+            }
+            return operatingSystem;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
